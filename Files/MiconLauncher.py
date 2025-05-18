eae73eab0a6536111c27164026b2e9f015520288
alessioiskuhl <109654241+alessioiskuhl@users.noreply.github.com>
2025-05-18 18:32:04 +0000

    Added the folder creation and adding of mods

--- Files/MiconLauncher.py
+++ Files/MiconLauncher.py
@@ -1,8 +1,14 @@
-from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint, QRect, QSize, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient, QLocale)
-from PySide2.QtWidgets import *
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QRadioButton, QTabWidget, QGroupBox, QComboBox, QTableWidget, QTableWidgetItem, QTextEdit, QLineEdit, QStatusBar, QFileDialog, QMessageBox
+from PyQt5.QtGui import QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient
+from PyQt5.QtCore import Qt, QSize, QLocale, QRect, QMetaObject, QMimeData
+import sys
+import os
+import shutil
 
-EnableAddModv = 0
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 
 class Ui_MiconLauncherWindow(object):
     def setupUi(self, MiconLauncherWindow):
@@ -10,7 +16,9 @@ class Ui_MiconLauncherWindow(object):
         if MiconLauncherWindow.objectName():
             MiconLauncherWindow.setObjectName(u"MiconLauncherWindow")
         MiconLauncherWindow.resize(673, 550)
-        MiconLauncherWindow.setMinimumSize(QSize(673, 502))
+        MiconLauncherWindow.setMinimumSize(QSize(673, 550))
+        MiconLauncherWindow.setMaximumSize(QSize(673, 550))
+        MiconLauncherWindow.setWindowTitle("MiconLauncher")
         MiconLauncherWindow.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
         MiconLauncherWindow.setAnimated(True)
         MiconLauncherWindow.setTabShape(QTabWidget.Rounded)
@@ -33,13 +41,6 @@ class Ui_MiconLauncherWindow(object):
         self.MinecraftOptionsComment.setObjectName(u"MinecraftOptionsComment")
         self.MinecraftOptionsComment.setGeometry(QRect(10, 160, 661, 291))
 
-        #Radio Button for selection of Vanilla Minecraft
-        self.VanillaSelect = QRadioButton(self.MinecraftOptionsComment)
-        self.VanillaSelect.setObjectName(u"VanillaSelect")
-        self.VanillaSelect.setGeometry(QRect(10, 80, 82, 17))
-        if self.VanillaSelect.isChecked():
-            EnableAddModv = 0
-
         #Minecraft version selection
         self.MinecraftVersionAuswahl_1 = QComboBox(self.MinecraftOptionsComment)
         self.MinecraftVersionAuswahl_1.addItem(u"Minecraft Version")
@@ -52,22 +53,20 @@ class Ui_MiconLauncherWindow(object):
         self.MinecraftVersionAuswahlLabel_1.setObjectName(u"MinecraftVersionAuswahlLabel_1")
         self.MinecraftVersionAuswahlLabel_1.setGeometry(QRect(10, 43, 261, 16))
 
-        #Radio Button for selection of Forge Minecraft
-        self.ForgeSelect = QRadioButton(self.MinecraftOptionsComment)
-        self.ForgeSelect.setObjectName(u"ForgeSelect")
-        self.ForgeSelect.setGeometry(QRect(100, 80, 82, 17))
-        if self.ForgeSelect.isChecked():
-            EnableAddModv = 1
+        # Create radio buttons
+        self.VanillaSelect = QRadioButton("Vanilla", self.MinecraftOptionsComment)
+        self.ForgeSelect = QRadioButton("Forge", self.MinecraftOptionsComment)
+        self.FabricSelect = QRadioButton("Fabric", self.MinecraftOptionsComment)
 
-        #Radio Button for selection of Fabric Minecraft
-        self.FabricSelect = QRadioButton(self.MinecraftOptionsComment)
-        self.FabricSelect.setObjectName(u"FabricSelect")
+        self.VanillaSelect.setGeometry(QRect(10, 80, 82, 17))
+        self.ForgeSelect.setGeometry(QRect(100, 80, 82, 17))
         self.FabricSelect.setGeometry(QRect(190, 80, 82, 17))
-        if self.FabricSelect.isChecked():
-            EnableAddModv = 1
+
+
+
 
         #Mods Table for addition of mods
-        self.ModsTable = QTableWidget(self.MinecraftOptionsComment)
+        self.ModsTable = DragDropTable(self.MinecraftOptionsComment)
         if (self.ModsTable.columnCount() < 3):
             self.ModsTable.setColumnCount(3)
         font = QFont()
@@ -80,11 +79,24 @@ class Ui_MiconLauncherWindow(object):
         __qtablewidgetitem2 = QTableWidgetItem()
         self.ModsTable.setHorizontalHeaderItem(2, __qtablewidgetitem2)
         self.ModsTable.setObjectName(u"ModsTable")
-        if EnableAddModv == 0:
-            self.ModsTable.setEnabled(False)
-        else:
-            self.ModsTable.setEnabled(True)
         self.ModsTable.setGeometry(QRect(10, 110, 541, 161))
+
+        # Disable initially if Vanilla is default
+        self.ModsTable.setEnabled(False)
+        
+        # Function to update table state
+        def updateModsTableState():
+            if self.VanillaSelect.isChecked():
+                self.ModsTable.setEnabled(False)
+            else:
+                self.ModsTable.setEnabled(True)
+        
+        # Connect signals
+        self.VanillaSelect.toggled.connect(updateModsTableState)
+        self.ForgeSelect.toggled.connect(updateModsTableState)
+        self.FabricSelect.toggled.connect(updateModsTableState)
+
+        
 
         #A Comment to mark the General Settings area
         self.GeneralSettingComment = QGroupBox(self.CreateTab)
@@ -118,6 +130,7 @@ class Ui_MiconLauncherWindow(object):
         self.BrowseButton_1 = QPushButton(self.GeneralSettingComment)
         self.BrowseButton_1.setObjectName(u"BrowseButton_1")
         self.BrowseButton_1.setGeometry(QRect(480, 98, 75, 23))
+        
 
         #A label and a drop down box to choose the used language
         self.LanguageSelectionLabel = QLabel(self.CreateTab)
@@ -246,51 +259,155 @@ class Ui_MiconLauncherWindow(object):
     # setupUi
 
     def retranslateUi(self, MiconLauncherWindow):
-        MiconLauncherWindow.setWindowTitle(QCoreApplication.translate("MiconLauncherWindow", u"MiconLauncher", None))
-        self.MinecraftOptionsComment.setTitle(QCoreApplication.translate("MiconLauncherWindow", u"Minecraft options", None))
-        self.VanillaSelect.setText(QCoreApplication.translate("MiconLauncherWindow", u"Vanilla", None))
+        MiconLauncherWindow.setWindowTitle(QApplication.translate("MiconLauncherWindow", u"MiconLauncher", None))
+        self.MinecraftOptionsComment.setTitle(QApplication.translate("MiconLauncherWindow", u"Minecraft options", None))
+        self.VanillaSelect.setText(QApplication.translate("MiconLauncherWindow", u"Vanilla", None))
 
-        self.MinecraftVersionAuswahlLabel_1.setText(QCoreApplication.translate("MiconLauncherWindow", u"Choose the Minecraft Version you want to use", None))
-        self.ForgeSelect.setText(QCoreApplication.translate("MiconLauncherWindow", u"Forge", None))
-        self.FabricSelect.setText(QCoreApplication.translate("MiconLauncherWindow", u"Fabric", None))
+        self.MinecraftVersionAuswahlLabel_1.setText(QApplication.translate("MiconLauncherWindow", u"Choose the Minecraft Version you want to use", None))
+        self.ForgeSelect.setText(QApplication.translate("MiconLauncherWindow", u"Forge", None))
+        self.FabricSelect.setText(QApplication.translate("MiconLauncherWindow", u"Fabric", None))
         ___qtablewidgetitem = self.ModsTable.horizontalHeaderItem(0)
-        ___qtablewidgetitem.setText(QCoreApplication.translate("MiconLauncherWindow", u"File name", None));
+        ___qtablewidgetitem.setText(QApplication.translate("MiconLauncherWindow", u"File name", None));
         ___qtablewidgetitem1 = self.ModsTable.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("MiconLauncherWindow", u"File size", None));
+        ___qtablewidgetitem1.setText(QApplication.translate("MiconLauncherWindow", u"File size", None));
         ___qtablewidgetitem2 = self.ModsTable.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("MiconLauncherWindow", u"File path", None));
-        self.GeneralSettingComment.setTitle(QCoreApplication.translate("MiconLauncherWindow", u"General Settings", None))
-        self.FolderNameEdit_1.setHtml(QCoreApplication.translate("MiconLauncherWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        ___qtablewidgetitem2.setText(QApplication.translate("MiconLauncherWindow", u"File path", None));
+        self.GeneralSettingComment.setTitle(QApplication.translate("MiconLauncherWindow", u"General Settings", None))
+        self.FolderNameEdit_1.setHtml(QApplication.translate("MiconLauncherWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Folder name</p></body></html>", None))
-        self.FolderNameLabel_1.setText(QCoreApplication.translate("MiconLauncherWindow", u"Name the folder in which the server is located", None))
-        self.ServerNameEdit_1.setHtml(QCoreApplication.translate("MiconLauncherWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.FolderNameLabel_1.setText(QApplication.translate("MiconLauncherWindow", u"Name the folder in which the server is located", None))
+        self.ServerNameEdit_1.setHtml(QApplication.translate("MiconLauncherWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Server name</p></body></html>", None))
-        self.ServerNameLabel_1.setText(QCoreApplication.translate("MiconLauncherWindow", u"Name the server you want to create", None))
-        self.FolderPathLabel_1.setText(QCoreApplication.translate("MiconLauncherWindow", u"Choose the path where the server folder will be put", None))
-        self.BrowseButton_1.setText(QCoreApplication.translate("MiconLauncherWindow", u"Browse", None))
-        self.LanguageSelectionLabel.setText(QCoreApplication.translate("MiconLauncherWindow", u"Select a language:", None))
+        self.ServerNameLabel_1.setText(QApplication.translate("MiconLauncherWindow", u"Name the server you want to create", None))
+        self.FolderPathLabel_1.setText(QApplication.translate("MiconLauncherWindow", u"Choose the path where the server folder will be put", None))
+        self.BrowseButton_1.setText(QApplication.translate("MiconLauncherWindow", u"Browse", None))
+        self.LanguageSelectionLabel.setText(QApplication.translate("MiconLauncherWindow", u"Select a language:", None))
 
-        self.CreateButton.setText(QCoreApplication.translate("MiconLauncherWindow", u"Start creating", None))
-        self.Tabs.setTabText(self.Tabs.indexOf(self.CreateTab), QCoreApplication.translate("MiconLauncherWindow", u"Create", None))
-        self.StartButton.setText(QCoreApplication.translate("MiconLauncherWindow", u"Start", None))
-        self.StopButton.setText(QCoreApplication.translate("MiconLauncherWindow", u"Stop", None))
-        self.FolderSelection_1.setText(QCoreApplication.translate("MiconLauncherWindow", u"Select the Folder containing the server files (Self-made or edited", None))
-        self.FolderSelection_2.setText(QCoreApplication.translate("MiconLauncherWindow", u"servers may not work, select folder made by this tool)", None))
-        self.BrowseButton_2.setText(QCoreApplication.translate("MiconLauncherWindow", u"Browse", None))
+        self.CreateButton.setText(QApplication.translate("MiconLauncherWindow", u"Start creating", None))
+        self.Tabs.setTabText(self.Tabs.indexOf(self.CreateTab), QApplication.translate("MiconLauncherWindow", u"Create", None))
+        self.StartButton.setText(QApplication.translate("MiconLauncherWindow", u"Start", None))
+        self.StopButton.setText(QApplication.translate("MiconLauncherWindow", u"Stop", None))
+        self.FolderSelection_1.setText(QApplication.translate("MiconLauncherWindow", u"Select the Folder containing the server files (Self-made or edited", None))
+        self.FolderSelection_2.setText(QApplication.translate("MiconLauncherWindow", u"servers may not work, select folder made by this tool)", None))
+        self.BrowseButton_2.setText(QApplication.translate("MiconLauncherWindow", u"Browse", None))
         ___qtablewidgetitem3 = self.AddModsTable.horizontalHeaderItem(0)
-        ___qtablewidgetitem3.setText(QCoreApplication.translate("MiconLauncherWindow", u"File name", None));
+        ___qtablewidgetitem3.setText(QApplication.translate("MiconLauncherWindow", u"File name", None));
         ___qtablewidgetitem4 = self.AddModsTable.horizontalHeaderItem(1)
-        ___qtablewidgetitem4.setText(QCoreApplication.translate("MiconLauncherWindow", u"File size", None));
+        ___qtablewidgetitem4.setText(QApplication.translate("MiconLauncherWindow", u"File size", None));
         ___qtablewidgetitem5 = self.AddModsTable.horizontalHeaderItem(2)
-        ___qtablewidgetitem5.setText(QCoreApplication.translate("MiconLauncherWindow", u"File path", None));
-        self.AddModsLabel.setText(QCoreApplication.translate("MiconLauncherWindow", u"Add more mods here:", None))
-        self.Tabs.setTabText(self.Tabs.indexOf(self.ManageTab), QCoreApplication.translate("MiconLauncherWindow", u"Manage", None))
+        ___qtablewidgetitem5.setText(QApplication.translate("MiconLauncherWindow", u"File path", None));
+        self.AddModsLabel.setText(QApplication.translate("MiconLauncherWindow", u"Add more mods here:", None))
+        self.Tabs.setTabText(self.Tabs.indexOf(self.ManageTab), QApplication.translate("MiconLauncherWindow", u"Manage", None))
     # retranslateUi
 
 
+class DragDropTable(QTableWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+
+    def dragMoveEvent(self, event):
+        event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            for url in event.mimeData().urls():
+                file_path = url.toLocalFile()
+
+                if os.path.isfile(file_path):
+                    file_name = os.path.basename(file_path)
+                    file_size = os.path.getsize(file_path)
+
+                    row = self.rowCount()
+                    self.insertRow(row)
+
+                    self.setItem(row, 0, QTableWidgetItem(file_name))
+                    self.setItem(row, 1, QTableWidgetItem(str(file_size) + " bytes"))
+                    self.setItem(row, 2, QTableWidgetItem(file_path))
+                    self.setItem(row, 3, QTableWidgetItem(file_path))
+
+                    # Copy the file to a 'mods' folder
+                    desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+                    target_folder = os.path.join(desktop, "MiconLauncher_Mods")
+                    os.makedirs(target_folder, exist_ok=True)
+                    shutil.copy(file_path, os.path.join(target_folder, file_name))
+    
+    
+
+    
+
+    
+
+# Main function
+class MiconLauncher(QtWidgets.QMainWindow):
+    def copy_files_from_table(self, Widget_table, folder_name, target_folder):
+        base_folder_name = folder_name.strip()
+        complete_folder_path = os.path.join(target_folder, base_folder_name)
+        counter = 1
+
+        # Add a suffix if the folder already exists
+        while os.path.exists(complete_folder_path):
+            complete_folder_path = os.path.join(target_folder, f"{base_folder_name}_{counter}")
+            counter += 1
+
+        # Now create the uniquely named folder
+        os.makedirs(complete_folder_path)
+    
+        for row in range(Widget_table.rowCount()):
+            item = Widget_table.item(row, 2)  # column 2 = file_path
+            if item:
+                file_path = item.text()
+                if os.path.isfile(file_path):  # Ensure it's a valid file
+                    try:
+                        shutil.copy(file_path, complete_folder_path)
+                        QMessageBox.information(self, "Done", "Files have been copied successfully.")
+                    except Exception as e:
+                        print(f"Failed to copy {file_path}: {e}")
+
+    def startCreating(self):
+        folder_copy_name = self.ui.FolderNameEdit_1.toPlainText()
+        folder_copy_path = self.ui.FolderPathText_1.text()
+        if not folder_copy_path:
+            QMessageBox.warning(self, "No folder", "Please select a target folder.")
+            return
+
+        os.makedirs(folder_copy_path, exist_ok=True)
+
+        if os.path.isdir(folder_copy_path):
+            self.copy_files_from_table(self.ui.ModsTable, folder_copy_name, folder_copy_path)
+        else:
+            print("Invalid target folder.")
+
+        
+
+    def browseFolder(self):
+            folder = QFileDialog.getExistingDirectory(self, "Select Folder")
+            if folder:
+                self.ui.FolderPathText_1.setText(folder)
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MiconLauncherWindow()
+        self.ui.setupUi(self)
+
+        self.ui.BrowseButton_1.clicked.connect(self.browseFolder)
+        self.ui.CreateButton.clicked.connect(self.startCreating)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MiconLauncher()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
